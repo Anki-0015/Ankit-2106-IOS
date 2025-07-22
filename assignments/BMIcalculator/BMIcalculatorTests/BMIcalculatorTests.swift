873ce5d48046c57c35309c6db94138e33607f59c
Ankit Bansal <bansalankit1575@gmail.com>
2025-07-22 09:49:08 +0530

    assignments upload 1

---
+++ assignments/BMIcalculator/BMIcalculatorTests/BMIcalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMIcalculatorTests.swift
+//  BMIcalculatorTests
+//
+//  Created by Ankit bansal on 14/07/25.
+//
+
+import Testing
+@testable import BMIcalculator
+
+struct BMIcalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
