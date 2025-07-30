be41cdcbe49f84b1ce699f6e7c32fdb5d0c1d00b
Ankit Bansal <bansalankit1575@gmail.com>
2025-07-30 10:14:35 +0530

    Assignments upload

---
+++ apps xcode/Class Apps/ApplePie_Practice/ApplePie_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie_Practice
+//
+//  Created by Ankit bansal on 28/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ apps xcode/Class Apps/Calculator_practice/Calculator_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator_practice
+//
+//  Created by Ankit bansal on 28/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
